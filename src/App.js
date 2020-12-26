7d83e95e21ed3843cc0fa9068bd73b73812cad76
Alex-Eojin <kimeojin1995@gmail.com>
2020-12-27 01:49:37 +0900

    리액트 라우터 URL 파라머티를 사용한 관리

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import React, { useCallback, useState } from 'react';
-import Categories from './components/Categories';
-import NewsList from './components/NewsList';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewPage';
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), []);
-  return (
-    <div>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </div>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
